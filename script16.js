59a545325a01684aefdc252d926e3920128b637d
KevinRamlow <kevinandraderamlow@gmail.com>
2023-06-25 03:02:22 -0300

    a

---
+++ script16.js
@@ -0,0 +1,16 @@
+function clicar() {
+    multiplicationTable.innerHTML = ''
+    var txtNumber = document.getElementById('number')
+    number = Number(txtNumber.value)
+
+    if (txtNumber.value == '') {
+        return window.alert('[ERRO] Faltam dados para prosseguir!')
+    }
+    
+    var multiplier = 1
+    for (var multiplier; multiplier <= 10; multiplier++) {
+        var result = number * multiplier
+
+        multiplicationTable.innerHTML += `${number} x ${multiplier} = ${result} <br/>`
+    } 
+}
