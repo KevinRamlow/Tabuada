d40de69a771dcb276d25cc6844369f650a2a003f
KevinRamlow <kevinandraderamlow@gmail.com>
2023-11-27 22:03:13 -0300

    org

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 function clicar() {
-    multiplicationTable.innerHTML = ''
-    var txtNumber = document.getElementById('number')
-    number = Number(txtNumber.value)
+  multiplicationTable.innerHTML = "";
+  var txtNumber = document.getElementById("number");
+  number = Number(txtNumber.value);
 
-    if (txtNumber.value == '') {
-        return window.alert('[ERRO] Faltam dados para prosseguir!')
-    }
-    
-    var multiplier = 1
-    for (var multiplier; multiplier <= 10; multiplier++) {
-        var result = number * multiplier
+  if (txtNumber.value == "") {
+    return window.alert("[ERRO] Faltam dados para prosseguir!");
+  }
 
-        multiplicationTable.innerHTML += `${number} x ${multiplier} = ${result} <br/>`
-    } 
+  var multiplier = 1;
+  for (var multiplier; multiplier <= 10; multiplier++) {
+    var result = number * multiplier;
+    multiplicationTable.innerHTML += `${number} x ${multiplier} = ${result} <br/>`;
+  }
 }
